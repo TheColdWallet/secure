dd10fb46c86cc4c9655b508577fb0600cf4266a4
TheColdWallet <100329306+TheColdWallet@users.noreply.github.com>
2022-02-25 11:10:36 +0000

    Add files via upload

---
+++ wb16_avantage.js
@@ -0,0 +1,9 @@
+   
+   $(document).ready(function()
+   {
+      function skrollrInit()
+      {
+         skrollr.init({forceHeight: false, mobileCheck: function() { return false; }, smoothScrolling: false});
+      }
+      skrollrInit();
+   });
